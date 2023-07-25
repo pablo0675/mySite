1be9c4128355563a15c22a9816a2ba7d5878686d
pablal levy <pablo.levy@epitech.eu>
2023-07-26 00:22:08 +0200

    :sparkles:feat: give it from my daddy

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
-
+import React, { useEffect, useRef } from 'react';
+import music from '../assets/music.mp3';
 import '../pages/Home.css';
+
 const Home: React.FC = () => {
+    const audioRef = useRef<HTMLAudioElement | null>(null);
+
+    useEffect(() => {
+        const audio = audioRef.current;
+
+        if (!audio) return; // Return early if audio is null
+
+        const storedTime = localStorage.getItem('musicTime');
+
+        // Check if there's a stored time for the audio
+        if (storedTime) {
+            const parsedTime = parseFloat(storedTime);
+            if (!isNaN(parsedTime)) {
+                audio.currentTime = parsedTime;
+            }
+        }
+
+        // Save the current time of the audio to localStorage when component unmounts
+        return () => {
+            localStorage.setItem('musicTime', audio.currentTime.toString());
+        };
+    }, []);
     return (
-        <div
-            className="home-background">
-                <p className="home-text">
-                    I will destroy your ass
-                </p>
+        <div className="home-background">
+            <p className="home-text">I will destroy your ass</p>
+            <audio ref={audioRef} autoPlay loop>
+                <source src={music} type="audio/mpeg" />
+                Your browser does not support the audio element.
+            </audio>
         </div>
-);
+    );
 };
 
 export default Home;
